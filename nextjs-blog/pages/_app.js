8fa60e51ee3226200b7d974416be7a5f5f8a8d1a
most111 <44298945+most111@users.noreply.github.com>
2022-09-18 22:00:49 +0100

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,4 +1,12 @@
 import { SocketStoreProvider } from "../context/SocketStore.js";
+import { PlayerDetailsStoreProvider } from "../context/PlayerStore.js";
+import { CardStackStoreProvider } from "../context/CardStore.js";
+import { EventCardStoreProvider } from "../context/EventCardStore.js";
+import { EventStoreProvider } from "../context/EventStore.js";
+import { CardTriggerProvider } from "../context/CardTrigger.js";
+import { ItemStoreProvider } from "../context/ItemStore.js";
+import { ActiveCellStoreProvider } from "../context/ActiveCellStore.js";
+import { TestStoreProvider } from "../context/TestStore.js";
 import { PlayerListStoreProvider } from "../context/PlayerListStore.js";
 
 function MyApp({ Component, pageProps }) {
