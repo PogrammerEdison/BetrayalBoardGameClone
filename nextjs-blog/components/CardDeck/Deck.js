576696693dde0b77d7ffc23ca6c27d44a0e4a5c9
most111 <44298945+most111@users.noreply.github.com>
2022-09-18 22:26:34 +0100

    cahnge

--- nextjs-blog/components/CardDeck/Deck.js
+++ nextjs-blog/components/CardDeck/Deck.js
@@ -6,7 +6,7 @@ function Deck(props) {
   console.log("we made it");
   if(props.type == "placeCard") {
   return props.cards.slice(0).reverse().map((cardInfo) => //slice 0 creates shallow copy
-  <Card card={cardInfo} key={cardInfo.name}/>) 
+    <Card card={cardInfo} key={cardInfo.name}/>) 
   } else {
     return props.cards.slice(0).reverse().map((cardInfo) => //slice 0 creates shallow copy
     <EventStack card={cardInfo} key={cardInfo.name}/>) 
